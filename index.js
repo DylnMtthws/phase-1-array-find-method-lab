fddf76ae894eb14e96c0ef5fc8b34dcfda6034fa
dylanmatthews <djmatthews93@gmail.com>
2023-09-16 13:22:54 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  //first attempt - 'result' === 'W' will always return false 
+  // because that's comparing two strings, not record.result
+//   function superbowlWin(array) {
+//     if ('result' === 'W') {
+//         return 'year'
+//     }
+//   }
+//   console.log(record.find(superbowlWin))
+
+
+  function superbowlWin(array) {
+    const trophyOrNot = array.find(record => record.result === 'W')
+    return trophyOrNot ? trophyOrNot.year : undefined
+  }
+
+console.log(superbowlWin(record))
